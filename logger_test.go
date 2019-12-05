4dc74583c80067c0be93237faa0157e4c16516e9
ohko <ohko@qq.com>
2019-12-05 22:14:04 +0800

    .

--- logger_test.go
+++ logger_test.go
@@ -17,15 +17,13 @@ func TestNewLogger(t *testing.T) {
 	// l1.SetLevel(LoggerLevel3Fatal)
 	l1.SetPrefix("L1")
 
-	for {
-		l1.Log0Debug(fmt.Sprintf("0:%v", "Debug"))
-		l1.Log1Warn("1:Warning")
-		l1.Log2Error("2:Error")
-		l1.SetPrefix("l1")
-		// l1.Log3Fatal("3:Fatal")
-		l1.Log4Trace("4:Trace")
-		time.Sleep(time.Second)
-	}
+	l1.Log0Debug(fmt.Sprintf("0:%v", "Debug"))
+	l1.Log1Warn("1:Warning")
+	l1.Log2Error("2:Error")
+	l1.SetPrefix("l1")
+	// l1.Log3Fatal("3:Fatal")
+	l1.Log4Trace("4:Trace")
+	time.Sleep(time.Second)
 
 	l1.SetColor(false)
 	l1.Log1Warn("no color")
